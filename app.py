1d9aeb5589f19e9db3a6a111a6d563657703ffa0
RualGF <rualgf@gmail.com>
2025-06-02 22:30:11 +0200

    Cambios en los scripts de python para adaptarse a los de SQL

--- app.py
+++ app.py
@@ -1,41 +1,73 @@
 import pandas as pd
-import requests
-from time import sleep
 import streamlit as st
+from datetime import date
 
-st.set_page_config(
-    page_title="Ex-stream-ly Cool App",
-    page_icon="🧊",
-    layout="wide",
-    initial_sidebar_state="expanded",
-    menu_items={
-        "Get Help": "https://www.extremelycoolapp.com/help",
-        "Report a bug": "https://www.extremelycoolapp.com/bug",
-        "About": "# This is a header. This is an *extremely* cool app!",
-    },
-)
 
-st.title("Predicciones meteorológicas")
-st.divider()
+from src import conectar 
 
-df = pd.read_csv(r"D:\PROYECTO-FINAL-\data\provinvias.csv")
+def main():
+    conexion = conectar.conexion()
 
-with st.expander("Provincias"):
-    # with st.spinner("Cargando provincias..."):
-    #     sleep(5)
-    st.dataframe(df, hide_index=True)
 
-st.divider()
+    st.set_page_config(
+        page_title="Ex-stream-ly Cool App",
+        page_icon="🧊",
+        layout="wide",
+        initial_sidebar_state="expanded",
+        menu_items={
+            "Get Help": "https://www.extremelycoolapp.com/help",
+            "Report a bug": "https://www.extremelycoolapp.com/bug",
+            "About": "# This is a header. This is an *extremely* cool app!",
+        },
+    )
 
-opcion_elegida = st.sidebar.multiselect(
-    "Elige la provincia: ",
-    df["nombre"],
-    default=None,
-    placeholder="Elige las provincias que quieras ver"
-)
-st.sidebar.divider()
-st.sidebar.write(opcion_elegida)
-if opcion_elegida:
-    st.sidebar.write(opcion_elegida)
-    st.dataframe(df[df["nombre"].isin(opcion_elegida)], hide_index=True)
+    st.title("Predicciones meteorológicas")
+    st.divider()
+
+    df_provincias = pd.read_sql_table("provincias", conexion)
+    df_comunidades = pd.read_sql_table("comunidades_autonomas", conexion)
+
+    fecha = st.date_input(
+        "Selecciona una fecha:",
+        value=(date(2023, 5, 29), date(2025, 5, 28)),
+        min_value=date(2023, 5, 29),
+        max_value=date(2025, 5, 28),
+        format="DD/MM/YYYY"
+        )
+    st.write_stream(fecha)
+    opcion_comunidad = st.selectbox(
+        "Elige la comunidad: ",
+        df_comunidades["nombre"],
+        index=None,
+        placeholder="Elige una comunidad",
+        #format_func=lambda x: "Selecciona una comunidad..." if x == "" else x
+    )
+
+    st.divider()
+    if opcion_comunidad:
+        comunidad_elegida = df_comunidades[df_comunidades["nombre"] == opcion_comunidad]
+
+        provincias_filtradas = df_provincias[df_provincias["codigo_comunidad"] == comunidad_elegida["codigo_ine"].iloc[0]]
+        opcion_elegida = st.sidebar.multiselect(
+            "Elige la provincia: ",
+            provincias_filtradas["nombre"],
+            default=None,
+            placeholder="Elige las provincias que quieras ver"
+        )
+    else:
+        #opcion_elegida = None
+        opcion_elegida = st.sidebar.multiselect(
+        "Elige la provincia: ",
+        df_provincias["nombre"],
+        default=None,
+        placeholder="Elige las provincias que quieras ver"
+    )
+    st.sidebar.divider()
+
+    if opcion_elegida:
+        st.sidebar.write(opcion_elegida)
+        st.dataframe(df_provincias[df_provincias["nombre"].isin(opcion_elegida)], hide_index=True)
+
+if __name__ == "__main__":
+    main()
 
