38e1a244a7ad48d683d9a9e1e06cde94090ead3e
RualGF <rualgf@gmail.com>
2025-06-05 18:44:06 +0200

    Actualización para streamlit y del script de creación de tabla

--- data/crear_tabla_datos_meteorologicos.sql
+++ data/crear_tabla_datos_meteorologicos.sql
@@ -1,20 +1,19 @@
 CREATE TABLE datos_meteorologicos (
+    id_descarga VARCHAR(50) NOT NULL,
     fecha DATE NOT NULL,
     indicativo_estacion VARCHAR(10) NOT NULL,
     nombre_estacion VARCHAR(100),
     -- nombre_provincia VARCHAR(50), -- No la guardamos si ya tenemos codigo_provincia
     codigo_provincia TINYINT UNSIGNED, -- FK a provincias
     altitud SMALLINT UNSIGNED,
-    temperatura_media DECIMAL(4,1),
-    precipitacion DECIMAL(5,1),
-    temperatura_minima DECIMAL(4,1),
-    temperatura_maxima DECIMAL(4,1),
-    direccion_viento SMALLINT,
-    velocidad_media_viento DECIMAL(4,1),
-    racha_max_viento DECIMAL(4,1),
-    humedad_rel_media SMALLINT,
-    humedad_rel_max SMALLINT,
-    humedad_rel_min SMALLINT,
+    tmed DECIMAL(4,1),
+    tmin DECIMAL(4,1),
+    tmax DECIMAL(4,1),
+    prec DECIMAL(5,1),
+    velmedia DECIMAL(4,1),
+    racha DECIMAL(4,1),
+    hrMedia SMALLINT,
+    timestamp_extraccion DATETIME NOT NULL,
     PRIMARY KEY (fecha, indicativo_estacion), -- Clave primaria compuesta
     FOREIGN KEY (codigo_provincia) REFERENCES provincias(codigo_prov)
 );
