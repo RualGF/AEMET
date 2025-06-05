40ae64d8d7c0dcd702821eca75849c1b2cc6569e
RualGF <rualgf@gmail.com>
2025-06-05 18:50:38 +0200

    Actualización del formato de históricos

--- pages/1_Datos_historicos.py
+++ pages/1_Datos_historicos.py
@@ -27,19 +27,19 @@ def main():
             GROUP BY codigo_prov ; 
             """
         df = pd.read_sql_query(consulta, con = conexion)   
-        df.style.format(na_rep="Sin datos", decimal=",", thousands=".", precision=2) 
+        #df.style.format(na_rep="Sin datos", decimal=",", thousands=".", precision=2) 
         st.divider()
      
     st.dataframe(df, hide_index=True, use_container_width=True, column_config={
         "nombre": st.column_config.TextColumn("Nombre de la provincia"),
         "codigo_prov": None,
-        "AVG(d.altitud)": st.column_config.NumberColumn(label="Altitud media (m)"),
-        "AVG(d.tmed)": st.column_config.NumberColumn(label="Temp. media (ºC)", help="Temperatura media"),
-        "AVG(d.tmin)": st.column_config.NumberColumn(label="Temp. mínima (ºC)", help="Temperatura mínima"),
-        "AVG(d.tmax)": st.column_config.NumberColumn(label="Temp. máxima (ºC)", help="Temperatura máxima"),
-        "AVG(d.prec)": st.column_config.NumberColumn(label="Precip. media (mm)", help="Precipitación media"),
-        "AVG(d.racha) * 3.6": st.column_config.NumberColumn(label="Racha media (km/h)", help="Velocidad media del viento"),
-        "AVG(d.hrMedia)": st.column_config.NumberColumn(label="Humedad media (%)", help="Humedad relativa media")
+        "AVG(d.altitud)": st.column_config.NumberColumn(label="Altitud media (m)", format="%.2f"),
+        "AVG(d.tmed)": st.column_config.NumberColumn(label="Temp. media (ºC)", format="%.2f", help="Temperatura media"),
+        "AVG(d.tmin)": st.column_config.NumberColumn(label="Temp. mínima (ºC)", format="%.2f", help="Temperatura mínima"),
+        "AVG(d.tmax)": st.column_config.NumberColumn(label="Temp. máxima (ºC)", format="%.2f", help="Temperatura máxima"),
+        "AVG(d.prec)": st.column_config.NumberColumn(label="Precip. media (mm)", format="%.2f", help="Precipitación media"),
+        "AVG(d.racha) * 3.6": st.column_config.NumberColumn(label="Racha media (km/h)", format="%.2f", help="Velocidad media del viento"),
+        "AVG(d.hrMedia)": st.column_config.NumberColumn(label="Humedad media (%)", format="%.2f", help="Humedad relativa media")
     } )
 if __name__ == "__main__":
     main()
