74e266651136113a323b74c31f7c270d8c1bc196
augusto.salonio <augusto93921@gmail.com>
2025-06-07 15:32:37 +0200

    actualización rama matcheado con Raul

---
+++ data/crear_bd.sql
@@ -0,0 +1,9 @@
+CREATE DATABASE IF NOT EXISTS `AEMET`
+CHARACTER SET utf8mb4
+COLLATE utf8mb4_es_0900_ai_ci;
+
+USE aemet; 
+
+drop table datos_meteorologicos;
+drop table provincias;
+drop table comunidades;
