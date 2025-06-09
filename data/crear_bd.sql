d9209ac11c80045d10019bb05f4f63e674c1140b
augusto.salonio <augusto93921@gmail.com>
2025-06-09 14:23:55 +0200

    Actualización Augusto

--- data/crear_bd.sql
+++
@@ -1,9 +0,0 @@
-CREATE DATABASE IF NOT EXISTS `AEMET`
-CHARACTER SET utf8mb4
-COLLATE utf8mb4_es_0900_ai_ci;
-
-USE aemet; 
-
-drop table datos_meteorologicos;
-drop table provincias;
-drop table comunidades;
