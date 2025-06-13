76d69c1270586f7dc7e8e9ce4b2597b720de48dd
RualGF <rualgf@gmail.com>
2025-06-13 11:00:26 +0200

    Cambios de las credenciales para conectar a la base de datos

--- src/conectar.py
+++ src/conectar.py
@@ -7,11 +7,13 @@ def conexion():
     """Retorna una nueva conexión desde el motor."""
     usuario = st.secrets["database"]["user"]
     pw = st.secrets["database"]["password"]
-    bd = st.secrets["database"]["name"]
+    bd = st.secrets["database"]["dbname"]
     servidor = st.secrets["database"]["host"]
     puerto = st.secrets["database"]["port"]
 
-    motor = create_engine(f"mysql+pymysql://{usuario}:{pw}@{servidor}:{puerto}/{bd}")
+    #motor = create_engine(f"mysql+pymysql://{usuario}:{pw}@{servidor}:{puerto}/{bd}")
+    motor = create_engine(f"postgresql+psycopg2://{usuario}:{pw}@{servidor}:{puerto}/{bd}?sslmode=require")
     # Abrir una conexión
     conectar = motor.connect()
+    
     return conectar
